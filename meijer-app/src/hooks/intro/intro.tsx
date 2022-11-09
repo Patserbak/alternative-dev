eda9cbe7109065aec843a10a54094e17c56f35f6
Patserbak <florentmeijer@hotmail.com>
2022-11-09 12:00:04 +0100

    update

--- meijer-app/src/hooks/intro/intro.tsx
+++ meijer-app/src/hooks/intro/intro.tsx
@@ -1,9 +1,10 @@
+import Icon from '../icon/icon';
 import './intro.scss';
 
 const Intro = (): JSX.Element => {
     return (
         <div className="intro">
-            <span className="intro__logo">MEIJER</span>
+            <Icon classNameAdded="" />
         </div>
     );
 }
